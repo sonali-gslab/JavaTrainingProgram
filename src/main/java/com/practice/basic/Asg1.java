24cb5efa53327836fe2a8733b00522640f80fd25
sonali-gslab <sonali.gslab@turtlemint.com>
2023-12-13 14:05:11 +0530

    small change

--- src/main/java/com/practice/basic/Asg1.java
+++ src/main/java/com/practice/basic/Asg1.java
@@ -3,5 +3,6 @@ package com.practice.basic;
 public class Asg1 {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hello JAVA Training Programstatus");
     }
 }
